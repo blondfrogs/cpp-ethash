93bf043ba38efa874ee5bba7d55266149c830a40
blondfrogs <jeremy.anderson.utah@gmail.com>
2020-03-12 15:34:45 -0600

    Add ability to choose port

--- test/daemon/kawpow.cpp
+++ test/daemon/kawpow.cpp
@@ -4,72 +4,46 @@
 #include <ethash/ethash.hpp>
 #include <ethash/ethash-internal.hpp>
 #include <climits>
-
-
-//int main(int argc, const char* argv[])
-//{
-//    std::string str_header_hash;
-//    std::string str_mix_hash;
-//    std::string str_nonce;
-//    std::string str_boundary;
-//
-//    for (int i = 0; i < argc; ++i)
-//    {
-//        const std::string arg{argv[i]};
-//
-//        if (arg == "-h" && i + 1 < argc)
-//            str_header_hash = std::string(argv[++i]);
-//        else if (arg == "-m" && i + 1 < argc)
-//            str_mix_hash = std::string(argv[++i]);
-//        else if (arg == "-n" && i + 1 < argc)
-//            str_nonce = std::string(argv[++i]);
-//        else if (arg == "-b" && i + 1 < argc)
-//            str_boundary = std::string(argv[++i]);
-//    }
-//
-//    char final_hash[64];
-//
-//    if (progpow::light_verify(str_header_hash.c_str(), str_mix_hash.c_str(), str_nonce.c_str(), str_boundary.c_str(), final_hash))
-//        printf("%.*s\n", 64, final_hash);
-//    else {
-//        printf("Not found\n");
-//    }
-//
-//    return 0;
-//}
-
 #include <memory>
 #include <cstdint>
 #include <iostream>
 #include <evhttp.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h> /* strtoumax */
+#include <stdbool.h>
+#include <errno.h>
+
+#define DEFAULT_PORT 8799
+
+static bool str_to_uint16(const char *str, uint16_t *res);
+
 int main(int argc, const char* argv[])
 {
-    bool fLightMode = true;
-    bool fFullMode = false;
-
     static int epoch_number = 0;
-//    static ethash::epoch_context_full_ptr context_full {nullptr, nullptr};// = ethash::create_epoch_context_full(epoch_number);
+    std::uint16_t SrvPort = DEFAULT_PORT;
     static ethash::epoch_context_ptr context_light{nullptr, nullptr};
 
     for (int i = 0; i < argc; ++i)
     {
         const std::string arg{argv[i]};
 
-        if (arg == "-f") {
-            fFullMode = true;
-            fLightMode = false;
+        if (arg == "-p" && i + 1 < argc) {
+            if (!str_to_uint16(argv[++i], &SrvPort)) {
+                fprintf(stderr, "conversion error\n");
+                exit(2);
+            }
         }
 
         if (arg == "-e" && i + 1 < argc)
             epoch_number = atoi(argv[++i]);
     }
 
-
-    if (fFullMode) {
-        // TODO, not working yet
-    } else if (fLightMode) {
-        if (!context_light || context_light->epoch_number != epoch_number)
-            context_light = ethash::create_epoch_context(epoch_number);
+    // Build context
+    if (!context_light || context_light->epoch_number != epoch_number) {
+        std::cout << "Building context for epoch: " << epoch_number << std::endl;
+        context_light = ethash::create_epoch_context(epoch_number);
     }
 
     if (!event_init())
@@ -77,14 +51,19 @@ int main(int argc, const char* argv[])
         std::cerr << "Failed to init libevent." << std::endl;
         return -1;
     }
+
+    // Setup server
     char const SrvAddress[] = "127.0.0.1";
-    std::uint16_t SrvPort = 5555;
     std::unique_ptr<evhttp, decltype(&evhttp_free)> Server(evhttp_start(SrvAddress, SrvPort), &evhttp_free);
     if (!Server)
     {
         std::cerr << "Failed to init http server." << std::endl;
         return -1;
     }
+
+    std::cout << "Server started!" << std::endl;
+    std::cout << "Listening on port: " << SrvPort << std::endl;
+
     void (*OnReq)(evhttp_request *req, void *) = [] (evhttp_request *req, void *)
     {
         auto *OutBuf = evhttp_request_get_output_buffer(req);
@@ -92,16 +71,13 @@ int main(int argc, const char* argv[])
         if (!OutBuf)
             return;
 
+        // Construct the query struct
         struct evkeyvalq headers{};
         const struct evhttp_uri *uri = evhttp_request_get_evhttp_uri(req);
         const char * query = evhttp_uri_get_query(uri);
         evhttp_parse_query_str(query, &headers);
-//        printf("%s\n", evhttp_find_header(&headers, "header_hash"));
-//        printf("%s\n", evhttp_find_header(&headers, "mix_hash"));
-//        printf("%s\n", evhttp_find_header(&headers, "nonce"));
-//        printf("%s\n", evhttp_find_header(&headers, "height"));
-//        printf("%s\n", evhttp_find_header(&headers, "share_boundary"));
-//        printf("%s\n", evhttp_find_header(&headers, "block_boundary"));
+
+        // Get the headers from the query struct
         const char* header_hash_str = evhttp_find_header(&headers, "header_hash");
         const char* mix_hash_str = evhttp_find_header(&headers, "mix_hash");
         const char* nonce_str = evhttp_find_header(&headers, "nonce");
@@ -110,51 +86,50 @@ int main(int argc, const char* argv[])
         const char* block_boundary_str = evhttp_find_header(&headers, "block_boundary");
 
         if (!header_hash_str || !mix_hash_str || !nonce_str || !height_str || !share_boundary_str || !block_boundary_str) {
-            evbuffer_add_printf(OutBuf, "<html><body><center><h1>Invalid data. Missing required data</h1></center></body></html>");
-            evhttp_send_reply(req, HTTP_OK, "", OutBuf);
+            std::string error = "";
+
+            if (!header_hash_str)
+                error = "Invalid header_hash";
+            else if (!mix_hash_str)
+                error = "Invalid mix_hash";
+            else if (!nonce_str)
+                error = "Invalid nonce";
+            else if (!height_str)
+                error = "Invalid height";
+            else if (!share_boundary_str)
+                error = "Invalid share_boundary";
+            else if (!block_boundary_str)
+                error = "Invalid block_boundary";
+
+            evhttp_send_reply(req, HTTP_BADREQUEST, error.c_str(), OutBuf);
         } else {
             auto header_hash = to_hash256(header_hash_str);
             auto mix_hash = to_hash256(mix_hash_str);
             auto share_boundary = to_hash256(share_boundary_str);
             auto block_boundary = to_hash256(block_boundary_str);
 
+            // Convert nonce from string
             uint64_t nNonce;
             errno = 0;
             char *endp = nullptr;
             errno = 0; // strtoull will not set errno if valid
             unsigned long long int n = strtoull(nonce_str, &endp, 16);
             nNonce = (uint64_t) n;
-//            if (nNonce == 0 && endp == nonce_str) {
-//                std::cout << "Nonce wasn't a string" << std::endl;
-//                return;
-//            } else if (nNonce == ULLONG_MAX && errno) {
-//                std::cout << "Hex value was out of range" << std::endl;
-//                return;
-//            } else if (*endp) {
-//                std::cout << "Invalid hex string" << std::endl;
-//                return;
-//            }
 
+            // Convert height from string
             uint32_t nHeight;
             errno = 0;
             endp = nullptr;
             errno = 0; // strtoul will not set errno if valid
             unsigned long int nH = strtoul(height_str, &endp, 10);
             nHeight = (uint32_t) nH;
-//            if (nHeight == 0 && endp == height_str) {
-//                std::cout << "Nonce wasn't a string" << std::endl;
-//                return;
-//            } else if (nHeight == ULONG_MAX && errno) {
-//                std::cout << "Hex value was out of range" << std::endl;
-//                return;
-//            } else if (*endp) {
-//                std::cout << "Invalid hex string" << std::endl;
-//                return;
-//            }
 
+            // Check epoch number and context
             epoch_number = (int) nHeight / ETHASH_EPOCH_LENGTH;
-            if (!context_light || context_light->epoch_number != epoch_number)
+            if (!context_light || context_light->epoch_number != epoch_number) {
                 context_light = ethash::create_epoch_context(epoch_number);
+                std::cout << "Building new context for epoch: " << epoch_number << std::endl;
+            }
 
             const auto result = progpow::hash(*context_light, (int) nHeight, header_hash, nNonce);
             std::string share_met = "false";
@@ -188,4 +163,15 @@ int main(int argc, const char* argv[])
         return -1;
     }
     return 0;
+}
+
+static bool str_to_uint16(const char *str, uint16_t *res)
+{
+    char *end;
+    errno = 0;
+    intmax_t val = strtoimax(str, &end, 10);
+    if (errno == ERANGE || val < 0 || val > UINT16_MAX || end == str || *end != '\0')
+        return false;
+    *res = (uint16_t) val;
+    return true;
 }
